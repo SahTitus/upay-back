b6452b2c974048274e2d171c5c6d6011e0022c3c
SahTitus <89194958+SahTitus@users.noreply.github.com>
2022-09-06 09:39:08 +0000

    commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors(corsOptions));
-app.use(credentials);
+// app.use(credentials);
 
 app.use(express.urlencoded({ extended: false }));
 
